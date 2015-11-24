529173c2249d49fe760c7b377e47193d7bdb9dfb
James <code-for-coffee@users.noreply.github.com>
2015-11-24 12:12:56 -0600

    Update droplet_config.sh

--- droplet_config.sh
+++ droplet_config.sh
@@ -1,29 +1,15 @@
-#!/bin/bash
 apt-get update
-# updates the list of software our server knows about
-
 apt-get install ruby-dev -y
 apt-get install ruby -y
-# installs the tools we need for a ruby environment
-
 apt-get install build-essential -y
-# essential build tools such as GCC
-
 apt-get install git -y
-# what's going on with git right now?
-
-###--- postgres database!
 apt-get install postgresql -y
 apt-get install postgresql-contrib -y
-
-# create a user
 sudo -u postgres createuser -s $USER
 createdb $USER
 touch ~/.psql_history
 apt-get install libpq-dev -y
 gem install pg
-
-###--- gems!
 gem install json
 gem install bundler
 gem install sinatra
